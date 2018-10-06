b62daae8367bdcce9bf7937d88cf92f23f402b7a
Jatin Rampal <jatinrampal@hotmail.com>
2018-10-06 16:38:22 -0400

    Initial Commit

---
+++ MapKitAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKitAssignment
+//
+//  Created by Jatin Rampal on 2018-10-06.
+//  Copyright © 2018 Jatin Rampal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
